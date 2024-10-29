03ac1edfd1c3c9a553e12b455cbb0477371ae63e
Kushtrim <bujupikushtrim@outlook.com>
2024-10-29 13:14:18 +0100

    [refactor] tesing with placeholder json

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,3 +1,22 @@
-export default function page() {
-  return <h1>Cabins Page</h1>;
+import Counter from '../components/Counter';
+
+export default async function page() {
+  const res = await fetch('https://jsonplaceholder.typicode.com/users');
+  const data = await res.json();
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Cabins Page</h1>
+      <ul>
+        {data.map((user) => (
+          <li key={user.id}>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+          </li>
+        ))}
+      </ul>
+      <Counter users={data} />
+    </div>
+  );
 }

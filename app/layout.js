fdfb487c4088463713c4f0f7aec573ef236df808
Kushtrim <bujupikushtrim@outlook.com>
2024-10-29 22:26:58 +0100

    [refactor] added global styles and metadata

--- app/layout.js
+++ app/layout.js
@@ -1,15 +1,21 @@
-import Logo from './_components/Logo';
-import Navigation from './_components/Navigation';
+import Logo from '@/app/_components/Logo';
+import Navigation from '@/app/_components/Navigation';
+
+import '@/app/_styles/globals.css';
 
 export const metadata = {
-  title: 'The wild Oasis',
-  description: 'Developed by: Kushtrim Bujupi',
+  title: {
+    template: '%s | The Wild Oasis',
+    default: 'The Wild Oasis',
+  },
+  description:
+    'Luxuriour cabin hotel, located in the heart of the italian Dolomities. Surrounded by the most beautiful nature and dark forests. The perfect place for your next relaxation.',
 };
 
 export default function RootLayout({ children }) {
   return (
     <html lang='en'>
-      <body>
+      <body className='bg-primary-950 text-primary-100 min-h-screen'>
         <header>
           <Logo />
           <Navigation />

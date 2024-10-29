1c56ea2ecdb228023a22d45e37fa8f0c2a3fb2a9
Kushtrim <bujupikushtrim@outlook.com>
2024-10-29 22:24:33 +0100

    [refactor] adding metadata title

--- app/cabins/page.js
+++ app/cabins/page.js
@@ -1,22 +1,13 @@
-import Counter from '../_components/Counter';
+import Counter from '@/app/_components/Counter';
 
-export default async function page() {
-  const res = await fetch('https://jsonplaceholder.typicode.com/users');
-  const data = await res.json();
-  // console.log(data);
+export const metadata = {
+  title: 'Cabins',
+};
 
+export default function page() {
   return (
     <div>
       <h1>Cabins Page</h1>
-      <ul>
-        {data.map((user) => (
-          <li key={user.id}>
-            <h2>{user.name}</h2>
-            <p>{user.email}</p>
-          </li>
-        ))}
-      </ul>
-      <Counter users={data} />
     </div>
   );
 }

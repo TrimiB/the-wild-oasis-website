7bf6fc00a96761c2bdce9b8e5cc4635ff9b0d2c0
Kushtrim <bujupikushtrim@outlook.com>
2024-10-31 13:46:28 +0100

    [refactor] using the Image component form NextJS

--- app/_components/Logo.js
+++ app/_components/Logo.js
@@ -1,15 +1,19 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
+import logo from '@/public/logo.png';
 
 export default function Logo() {
   return (
     <Link
       href='/'
       className='flex items-center gap-4 z-10'>
-      <img
-        src='/logo.png'
+      <Image
+        src={logo}
         height='60'
         width='60'
         alt='The Wild Oasis logo'
+        quality={50}
       />
       <span className='text-xl font-semibold text-primary-100'>
         The Wild Oasis

382c660f4bf710aaa4f59b79604c65b23c1c4183
Kushtrim <bujupikushtrim@outlook.com>
2024-10-31 13:46:55 +0100

    [refactor] and styling

--- app/layout.js
+++ app/layout.js
@@ -17,10 +17,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body
-        className={`${font_Josefin_Sans.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+        className={`${font_Josefin_Sans.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col relative`}>
         <Header />
         <div className='flex-1 px-8 py-12'>
-          <main className='max-w-7xl mx-auto'>{children}</main>
+          <main className='max-w-7xl mx-auto mt-24'>{children}</main>
         </div>
       </body>
     </html>
--- app/page.js
+++ app/page.js
@@ -1,3 +1,8 @@
+import Image from 'next/image';
+import Link from 'next/link';
+
+import bg from '@/public/bg.png';
+
 export const metadata = {
   title: {
     template: '%s | The Wild Oasis',
@@ -7,10 +12,26 @@ export const metadata = {
 
 export default function Page() {
   return (
-    <main>
-      <div>
-        <h1>The Wild Oasis, Welcome to paradise</h1>
+    <>
+      <Image
+        src={bg}
+        alt='Mountains and forests with two cabins'
+        fill
+        placeholder='blur'
+        quality={80}
+        className='object-cover object-top'
+      />
+
+      <div className='relative z-10 text-center'>
+        <h1 className='text-8xl text-primary-50 mb-10 tracking-tight font-normal'>
+          Welcome to paradise.
+        </h1>
+        <Link
+          href='/cabins'
+          className='bg-accent-500 px-8 py-6 text-primary-800 text-lg font-semibold hover:bg-accent-600 transition-all'>
+          Explore luxury cabins
+        </Link>
       </div>
-    </main>
+    </>
   );
 }

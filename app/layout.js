c0cbf9ebd7f30b87df0a897d4b17bebd2bc03886
Kushtrim <bujupikushtrim@outlook.com>
2024-10-31 11:02:36 +0100

    [refactor] and styling

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,8 @@
-import Logo from '@/app/_components/Logo';
-import Navigation from '@/app/_components/Navigation';
-
-import '@/app/_styles/globals.css';
+import Header from './_components/Header';
 
 import { font_Josefin_Sans } from './_fonts/fonts';
+
+import '@/app/_styles/globals.css';
 
 export const metadata = {
   title: {
@@ -18,13 +17,11 @@ export default function RootLayout({ children }) {
   return (
     <html lang='en'>
       <body
-        className={`${font_Josefin_Sans.className} bg-primary-950 text-primary-100 min-h-screen`}>
-        <header>
-          <Logo />
-          <Navigation />
-        </header>
-        <main>{children}</main>
-        <footer>&copy; The Wild Oasis</footer>
+        className={`${font_Josefin_Sans.className} antialiased bg-primary-950 text-primary-100 min-h-screen flex flex-col`}>
+        <Header />
+        <div className='flex-1 px-8 py-12'>
+          <main className='max-w-7xl mx-auto'>{children}</main>
+        </div>
       </body>
     </html>
   );

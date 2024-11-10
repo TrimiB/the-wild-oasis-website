215080ccf445e98ccfc84f34650640463f8b63c1
Kushtrim <bujupikushtrim@outlook.com>
2024-11-10 20:56:10 +0100

    [new] error component to cact errors that might occur while loading

---
+++ app/error.js
@@ -0,0 +1,25 @@
+'use client';
+
+// import { useRouter } from 'next/navigation';
+
+export default function Error({ error, reset }) {
+  // const router = useRouter();
+  return (
+    <main className='flex justify-center items-center flex-col gap-6'>
+      <h1 className='text-3xl font-semibold'>Something went wrong!</h1>
+      <p className='text-lg'>{error.message}</p>
+
+      {/* <button
+        onClick={() => router.push('/')}
+        className='inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg'>
+        Try again
+      </button> */}
+
+      <button
+        onClick={() => reset()}
+        className='inline-block bg-accent-500 text-primary-800 px-6 py-3 text-lg'>
+        Try again
+      </button>
+    </main>
+  );
+}

a8aa95d63dcbe45cb725e26e1225b56bc5de2063
Kushtrim <bujupikushtrim@outlook.com>
2024-11-10 20:59:31 +0100

    [refactor] using generateStaticParams to generate the static paths for the cabin pages.

--- app/cabins/[cabinId]/page.js
+++ app/cabins/[cabinId]/page.js
@@ -1,19 +1,6 @@
-import { getCabin } from '@/app/_lib/data-service';
+import { getCabin, getCabins } from '@/app/_lib/data-service';
 import { EyeSlashIcon, MapPinIcon, UsersIcon } from '@heroicons/react/24/solid';
 import Image from 'next/image';
-
-// PLACEHOLDER DATA
-// const cabin = {
-//   id: 89,
-//   name: '001',
-//   maxCapacity: 2,
-//   regularPrice: 250,
-//   discount: 0,
-//   description:
-//     'Discover the ultimate luxury getaway for couples in the cozy wooden cabin 001. Nestled in a picturesque forest, this stunning cabin offers a secluded and intimate retreat. Inside, enjoy modern high-quality wood interiors, a comfortable seating area, a fireplace and a fully-equipped kitchen. The plush king-size bed, dressed in fine linens guarantees a peaceful nights sleep. Relax in the spa-like shower and unwind on the private deck with hot tub.',
-//   image:
-//     'https://dclaevazetcjjkrzczpc.supabase.co/storage/v1/object/public/cabin-images/cabin-001.jpg',
-// };
 
 export async function generateMetadata({ params }) {
   const { name } = await getCabin(params.cabinId);
@@ -22,6 +9,24 @@ export async function generateMetadata({ params }) {
     title: `Cabin ${name}`,
     description: `Cabin ${name}`,
   };
+}
+
+/**
+ * Generates the static parameters for the cabin pages.
+ *
+ * This function is used by Next.js to generate the static paths for the cabin pages.
+ * It retrieves all the cabins from the data service and maps them to an array of
+ * objects with the `cabinId` parameter.
+ * This array is then used by Next.js to generate the static paths for the cabin pages.
+ *
+ * @returns {Promise<{ cabinId: string }[]>} An array of objects with the `cabinId` parameter.
+ */
+export async function generateStaticParams() {
+  const cabins = await getCabins();
+
+  return cabins.map((cabin) => ({
+    cabinId: cabin.id.toString(),
+  }));
 }
 
 export default async function Page({ params }) {
@@ -53,14 +58,14 @@ export default async function Page({ params }) {
             <li className='flex gap-3 items-center'>
               <UsersIcon className='h-5 w-5 text-primary-600' />
               <span className='text-lg'>
-                For up to <span className='font-bold'>{maxCapacity}</span>{' '}
+                For up to <span className='font-bold'>{maxCapacity}</span>
                 guests
               </span>
             </li>
             <li className='flex gap-3 items-center'>
               <MapPinIcon className='h-5 w-5 text-primary-600' />
               <span className='text-lg'>
-                Located in the heart of the{' '}
+                Located in the heart of the
                 <span className='font-bold'>Dolomites</span> (Italy)
               </span>
             </li>

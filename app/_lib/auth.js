22ae960d0afca851afbfafe467b3fa44d2d396e4
Kushtrim <bujupikushtrim@outlook.com>
2024-11-23 22:50:30 +0100

    [new] auth.js configuration file with google as a provider and where we destruct handlers and authentication metods from NextAuth

---
+++ app/_lib/auth.js
@@ -0,0 +1,30 @@
+import NextAuth from 'next-auth';
+import Google from 'next-auth/providers/google';
+
+export const {
+  handlers: { GET, POST },
+  auth,
+  signIn,
+  signOut,
+} = NextAuth({
+  providers: [Google],
+  callbacks: {
+    authorized({ auth, request }) {
+      return !!auth;
+    },
+  },
+});
+
+// export const {
+//   handlers: { GET, POST },
+//   auth,
+//   signIn,
+//   signOut,
+// } = NextAuth({
+//   providers: [Google([
+//     {
+//       clientId: process.env.GOOGLE_CLIENT_ID,
+//       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+//     },
+//   ])],
+// });

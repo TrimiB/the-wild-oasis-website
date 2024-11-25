a545612023f397d0c904ffcb7caf31a570e0e521
Kushtrim <bujupikushtrim@outlook.com>
2024-11-25 22:15:14 +0100

    [refactor] signing in user. if no user then creating new user. Also setting user id to user id in user of session to get access to it from pages.

--- app/_lib/auth.js
+++ app/_lib/auth.js
@@ -1,5 +1,6 @@
 import NextAuth from 'next-auth';
 import Google from 'next-auth/providers/google';
+import { createGuest, getGuest } from './data-service';
 
 export const {
   handlers: { GET, POST },
@@ -11,6 +12,27 @@ export const {
   callbacks: {
     authorized({ auth, request }) {
       return !!auth;
+    },
+    async signIn({ user, account, profile }) {
+      try {
+        const existingGuest = await getGuest(user.email);
+
+        if (!existingGuest)
+          await createGuest({
+            email: user.email,
+            fullName: user.name,
+          });
+
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+    async session({ session, token }) {
+      const guest = await getGuest(session.user.email);
+      session.user.guestId = guest.id;
+      return session;
     },
   },
   pages: {

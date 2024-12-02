5e6a76764a1c3e50baac15a5e424f8e825cf1900
Kushtrim <bujupikushtrim@outlook.com>
2024-12-02 22:32:34 +0100

    [refactor] created ned action. Action takes formData as argument and extracts values form it to call the updateGuest function. Then revalidates the path.

--- app/_lib/actions.js
+++ app/_lib/actions.js
@@ -1,6 +1,33 @@
 'use server';
 
-import { signIn, signOut } from './auth';
+import { revalidatePath } from 'next/cache';
+import { auth, signIn, signOut } from './auth';
+import { updateGuest } from './data-service';
+
+export async function updateProfileAction(formData) {
+  const session = await auth();
+  if (!session) throw new Error('You must be logged in to update your profile');
+
+  if (!/^[a-zA-Z0-9]{6,18}$/.test(formData.get('nationalID'))) {
+    throw new Error('National ID number must be 6-18 characters long');
+  }
+
+  const nationalID = formData.get('nationalID');
+  const [nationality, countryFlag] = formData.get('nationality').split('%');
+
+  const updateData = {
+    // id: session.user.guestId,
+    // fullName: session.user.name,
+    // email: session.user.email,
+    nationality,
+    countryFlag,
+    nationalID,
+  };
+
+  updateGuest(session.user.guestId, updateData);
+
+  revalidatePath('/account/profile');
+}
 
 export async function loginAction() {
   await signIn('google', {

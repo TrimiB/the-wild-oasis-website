b8092d6080dc6684681fd869b7abb7d79e684ab5
Kushtrim <bujupikushtrim@outlook.com>
2024-12-06 13:50:34 +0100

    [refactor!] Removing ul and creating new ReservationList component. Also placing delete function there in a new useOptimistic hook.

--- app/_components/DeleteReservation.js
+++ app/_components/DeleteReservation.js
@@ -2,15 +2,14 @@
 
 import { TrashIcon } from '@heroicons/react/24/solid';
 import { useTransition } from 'react';
-import { DeleteReservationAction } from '../_lib/actions';
 import SpinnerMini from './SpinnerMini';
 
-function DeleteReservation({ bookingId }) {
+function DeleteReservation({ bookingId, onDelete }) {
   const [isPending, startTransition] = useTransition();
 
   function handleDelete() {
     startTransition(() => {
-      DeleteReservationAction(bookingId);
+      onDelete(bookingId);
     });
   }
 
